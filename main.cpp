ddfbc9c111ca2a10fa83ad78523c34786587f6e8
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-04-13 21:45:45 +0300

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 2021 Baklanov
-#include "TimedDoor.h"
-#include <iostream>
-
-int main() {
-    DoorTimeAdapter* a = new DoorTimeAdapter();
-    TimedDoor* door = new TimedDoor(a);
-    door->unlock();
-}
